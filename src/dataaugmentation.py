6d1f4823c15f8b78d80330b5922ef77eeb894fd1
Hazoom <hazoommo@gmail.com>
2019-10-30 16:30:12 +0200

    fix augmentation script

--- src/dataaugmentation.py
+++ src/dataaugmentation.py
@@ -7,7 +7,9 @@ def augment_data(input_file: str, output_file: str):
     sentences_df = pd.read_csv(input_file)
 
     all_companies = set(sentences_df['company'].to_list())
+    all_companies = {company for company in all_companies if isinstance(company, str)}
     all_products = set(sentences_df['product'].to_list())
+    all_products = {product for product in all_products if isinstance(product, str)}
 
     new_rows = []
     for _, row in sentences_df.iterrows():
@@ -18,10 +20,19 @@ def augment_data(input_file: str, output_file: str):
             # replace the company and the product
             random_company = str(random.choice(tuple(all_companies)))
             random_product = str(random.choice(tuple(all_products)))
-            sentence = row['sentence'].replace(str(row['company']), random_company).\
-                replace(str(row['product']), random_product)
 
-            new_rows.append([sentence, random_company, random_product])
+            sentence = str(row['sentence'])
+            new_company = row['company']
+            new_product = row['product']
+            if row['product'] is not None and str(row['product']) in sentence:
+                sentence = sentence.replace(str(row['product']), random_product)
+                new_product = random_product
+
+            if row['company'] is not None and str(row['company']) in sentence:
+                sentence = sentence.replace(str(row['company']), random_company)
+                new_company = random_company
+
+            new_rows.append([sentence, new_company, new_product])
 
     random.shuffle(new_rows)
 
